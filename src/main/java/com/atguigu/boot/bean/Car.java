742ac7930d0d70f947da1fe8ab207b9e0fa33a21
cxg <18838988633@163.com>
2022-04-09 00:47:10 +0800

    master commit11111

--- src/main/java/com/atguigu/boot/bean/Car.java
+++ src/main/java/com/atguigu/boot/bean/Car.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 
 /**
- * 只有在容器中的组件，才会拥有SpringBoot提供的强大功能cxgccc hello git4master
+ * 只有在容器中的组件，才会拥有SpringBoot提供的强大功能cxgccc hello git4master  1111
  * 只有在容器中的组件，才会拥有SpringBoot提供的强大功能cxgccc hello git4hotfix jiushisi push
  */
 @ToString
